12daabe89c6c3f28aab6db971ba131c069a7ad38
hypercubist <qpinguz@gmail.com>
2021-12-17 00:45:41 +0900

    211216 finish

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -2,55 +2,127 @@ import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
-
 interface RouteParams {
-    coinId: string;
+  coinId: string;
 }
 
-interface RouteState{
-    name : string;
+interface RouteState {
+  name: string;
 }
 
 const Container = styled.div`
-    max-width: 480px;
-    margin : 0 auto;
-    padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+  padding: 0 20px;
 `;
 
 const Header = styled.header`
-    height: 10vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Title = styled.h1`
-    font-size: 48px;
-    color:${props => props.theme.accentColor};
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
 `;
 
 const Loader = styled.span`
-    text-align: center;
-    font-size: 30px;
-    display: block;
+  text-align: center;
+  font-size: 30px;
+  display: block;
 `;
 
-function Coin(){
-    const [loading, setLoading] = useState(true);
-    const { coinId }= useParams<RouteParams>();
-    const {state} = useLocation<RouteState>();
-    useEffect(()=>{
-        (async()=>{
-            const infoData = await(await fetch (`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
-            const priceData = await(await fetch (`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  contract: string;
+  platform: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: Date;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: Date;
+  last_data_at: Date;
+}
 
-    return(
-        <Container>
-            <Header>
-                <Title>{state?.name || "Loading.."}</Title> 
-            </Header>
-            {loading ? <Loader>Loading Coins...</Loader> : null}
-        </Container>
-    )
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: Date;
+  last_updated: Date;
+  quotes: Quotes;
+}
+
+export interface Quotes {
+  USD: Usd;
+}
+
+export interface Usd {
+  price: number;
+  volume_24h: number;
+  volume_24h_change_24h: number;
+  market_cap: number;
+  market_cap_change_24h: number;
+  percent_change_15m: number;
+  percent_change_30m: number;
+  percent_change_1h: number;
+  percent_change_6h: number;
+  percent_change_12h: number;
+  percent_change_24h: number;
+  percent_change_7d: number;
+  percent_change_30d: number;
+  percent_change_1y: number;
+  ath_price: number;
+  ath_date: Date;
+  percent_from_price_ath: number;
+}
+
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState({});
+  const [price, setPriceInfo] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      console.log(infoData);
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      console.log(priceData);
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, [coinId]);
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading.."}</Title>
+      </Header>
+      {loading ? <Loader>Loading Coins...</Loader> : null}
+    </Container>
+  );
 }
 export default Coin;
